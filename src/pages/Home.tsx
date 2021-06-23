76863f7efeca1753079c80c7e4363466268510fb
dmitryrocha <dmitryrocha@gmail.com>
2021-06-23 16:33:23 -0300

    aula01

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+export function Home() {
+    return(
+        <div>
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de perguntas e respostas ao vivo</strong>
+                <p>Tire dúvidas da audiência em tempo real</p>
+            </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="Logo letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>ou entre em uma sala</div>
+                    <form>
+                        <input type="text" placeholder="Digite o código da sala" />
+                        <button type="submit"/>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
